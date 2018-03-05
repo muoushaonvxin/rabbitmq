9284db59e9f2fdc61c71b95723d6bec7f0c50b14
zhangyz <zhangyuzeoffice@163.com>
2018-03-05 14:15:06 +0800

    new commit

---
+++ scripts/direct/receive_test02.py
@@ -0,0 +1,17 @@
+import pika
+import sys
+
+credentials = pika.PlainCredentials("hitrader", "hitrader123")
+connection = pika.BlockingConnection(pika.ConnectionParameters('10.203.11.234', 5672, '/', credentials))
+channel = connection.channel()
+channel.exchange_declare(exchange='test',exchange_type='direct',durable=True)
+result = channel.queue_declare(exclusive=True)
+queue_name = "test02"
+
+channel.queue_bind(exchange='test',queue=queue_name,routing_key='')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r" % (body))
+
+channel.basic_consume(callback,queue=queue_name,no_ack=True)
+channel.start_consuming()
