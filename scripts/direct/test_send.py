751897c1465882c9eaa1741a5ccdfa2169c23d37
zhangyz <zhangyuzeoffice@163.com>
2018-04-25 21:47:48 +0800

    commit

--- scripts/direct/test_send.py
+++ scripts/direct/test_send.py
@@ -7,7 +7,7 @@ channel = connection.channel()
 
 message = "hello world!"
 
-for i in range(10000):
+for i in range(1):
 	channel.exchange_declare(exchange='test',exchange_type='direct', durable=True)
 	channel.basic_publish(exchange='test',routing_key='',body=message)
 connection.close()
